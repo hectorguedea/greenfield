0fa02fc231039d34d8e76e27247e0b4d03e69f6a
NOVELA VEGA JAVIER DASSAEV <javier.novela@ine.mx>
2022-02-09 13:08:57 -0600

    Primera Revisión

---
+++ custom.js
@@ -0,0 +1,8 @@
+  // ANIMACIÓN MENÚ MOBILE
+
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav");
+
+  hamburger.addEventListener("click", () => {
+      nav.classList.toggle("open");
+  })
